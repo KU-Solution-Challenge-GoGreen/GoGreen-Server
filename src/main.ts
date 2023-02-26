9435303f17c049cb628e837ebb985fedb5e5b130
worjs <whworjs777@gmail.com>
2023-02-26 17:08:44 +0900

    setup class-validator & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      skipUndefinedProperties: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  // swagger 세팅
+  const config = new DocumentBuilder()
+    .setTitle('GoGreen Server')
+    .setDescription('GoGreen API description')
+    .setVersion('1.0')
+    .addTag('GoGreen')
+    .addBearerAuth()
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
