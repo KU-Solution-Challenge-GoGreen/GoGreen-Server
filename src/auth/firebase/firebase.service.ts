6785fce8f3b91fae13a02343f8e686627b9b933b
worjs <whworjs777@gmail.com>
2023-02-28 18:21:57 +0900

    apply configModule for init firebase

--- src/auth/firebase/firebase.service.ts
+++ src/auth/firebase/firebase.service.ts
@@ -1,19 +1,11 @@
-import * as admin from 'firebase-admin';
-import { ConfigService } from '@nestjs/config';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { FirebaseAdmin } from './interface/firebase-admin.interface';
 
 @Injectable()
 export class FirebaseService {
-  private readonly firebaseAdmin: FirebaseAdmin;
-
-  constructor(private readonly config: ConfigService) {
-    this.firebaseAdmin = admin.initializeApp({
-      credential: admin.credential.cert(
-        require('../../../firebase-account.json'),
-      ),
-    });
-  }
+  constructor(
+    @Inject('FirebaseAdmin') private readonly firebaseAdmin: FirebaseAdmin,
+  ) {}
 
   async verifyToken(idToken: string): Promise<string> {
     return this.firebaseAdmin
