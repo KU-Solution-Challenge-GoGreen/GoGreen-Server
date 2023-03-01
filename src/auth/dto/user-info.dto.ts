ab8c03a6d59dd108d05b87b9b2937c1e8888429d
worjs <whworjs777@gmail.com>
2023-03-02 00:11:47 +0900

    UserInfoDto

--- src/auth/dto/user-info.dto.ts
+++ src/auth/dto/user-info.dto.ts
@@ -1,1 +1,41 @@
-export class UserInfoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+class VeganTypeDto {
+  @ApiProperty({
+    type: String,
+    description: '비건 타입 Id',
+  })
+  id!: string;
+
+  @ApiProperty({
+    type: String,
+    description: '비건 타입 이름',
+  })
+  name!: string;
+}
+
+export class UserInfoDto {
+  @ApiProperty({
+    type: String,
+    description: '유저 Id',
+  })
+  id!: string;
+
+  @ApiProperty({
+    type: String,
+    description: '닉네임',
+  })
+  name!: string;
+
+  @ApiProperty({
+    type: VeganTypeDto,
+    description: '비건 타입',
+  })
+  veganType!: VeganTypeDto;
+
+  @ApiProperty({
+    type: String,
+    description: '유저 사진',
+  })
+  photo!: string | null;
+}
