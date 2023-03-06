8ee5f789fb87b51812657b8077a3a9a8447a4403
worjs <whworjs777@gmail.com>
2023-03-07 01:07:51 +0900

    connect Ingredient Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { configModule } from './modules/config.module';
 import { CommonModule } from '../common/common.module';
 import { AuthModule } from '../auth/auth.module';
 import { FirebaseModule } from '../auth/firebase/firebase.module';
+import { IngredientModule } from '../ingredient/ingredient.module';
 
 @Module({
-  imports: [configModule, CommonModule, FirebaseModule, AuthModule],
+  imports: [
+    configModule,
+    CommonModule,
+    FirebaseModule,
+    AuthModule,
+    IngredientModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule implements NestModule {
