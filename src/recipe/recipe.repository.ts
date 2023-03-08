f1a25122f9f3f7da34c5d07f1098db2ee98f6c1c
worjs <whworjs777@gmail.com>
2023-03-08 22:56:37 +0900

    implement method

--- src/recipe/recipe.repository.ts
+++ src/recipe/recipe.repository.ts
@@ -60,6 +60,51 @@ export class RecipeRepository {
     };
   }
 
+  async isRecipeExist(recipeId: string): Promise<boolean> {
+    return (
+      (await this.prisma.recipe.count({
+        where: {
+          id: recipeId,
+          deletedAt: null,
+        },
+      })) > 0
+    );
+  }
+
+  async toggleRecipeBookmark(
+    userId: string,
+    recipeId: string,
+  ): Promise<boolean> {
+    //bookmark되어있으면 삭제, 아니면 생성
+    const bookmark = await this.prisma.recipeBookmark.findFirst({
+      where: {
+        userId,
+        recipeId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (bookmark) {
+      // (userId, recipeId)로 unique constraint 안 걸려있어서 배열로 타입이 추정되는 듯.
+      await this.prisma.recipeBookmark.delete({
+        where: {
+          id: bookmark.id,
+        },
+      });
+    } else {
+      await this.prisma.recipeBookmark.create({
+        data: {
+          userId,
+          recipeId,
+        },
+      });
+    }
+
+    return !bookmark;
+  }
+
   async getIngredientsByIds(ids: string[]): Promise<IngredientWithCategory[]> {
     return this.prisma.ingredient.findMany({
       where: {
